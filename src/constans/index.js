066236bf2e32cb11c5e20e904f2f7c2627bcbba6
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-09-11 19:13:04 +0200

    add constants

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,2 +1,4 @@
 export const FIFTEEN_MINUTES = 15 * 60 * 1000; //15 minutes in milliseconds
+// export const FIFTEEN_MINUTES = 60 * 1000;
+// export const FIFTEEN_MINUTES = 15 * 60 *60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000; //30 days in milliseconds
