71a55560fa1c97bb4dbd82110a33ead298bd094e
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-09-12 14:01:42 +0300

    add methods delete password in response

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -9,4 +9,11 @@ const usersSchema = new Schema(
   { timestamps: true, versionKey: false },
 );
 
-export const UsersCollection = model('users', usersSchema)
+
+usersSchema.methods.toJSON = function () {
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
+};
+
+export const UsersCollection = model('users', usersSchema);
