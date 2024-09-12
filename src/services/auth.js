482f778fa4df0e7692e996b07e47d59d3ec18e78
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-09-12 14:05:14 +0300

    encrypted password

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,11 @@
 import { UsersCollection } from "../db/models/user.js";
+import bcrypt from 'bcrypt';
 
 export const registerUser = async (payload) => {
-    return await UsersCollection.create(payload);
+    const encryptedPassword = await bcrypt.hash(payload.password, 10);
+    return await UsersCollection.create({
+        ...payload,
+        password: encryptedPassword,
+    });
 
 };
