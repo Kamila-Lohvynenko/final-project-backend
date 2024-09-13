798cb8e1b025acf9b7390741c788021bf3e0fa08
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-09-13 17:44:44 +0300

    Update multer.js

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -3,6 +3,7 @@ import { TEMP_UPLOAD_DIR } from '../constans/index.js';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, TEMP_UPLOAD_DIR)
-    }
+        cb(null, TEMP_UPLOAD_DIR);
+    },
+    filename: function ()
 })
