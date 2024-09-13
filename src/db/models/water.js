752173b3156d71fa1f4f936865f3f716400ed3d7
Lesia Sasiuk <chumachocl@gmail.com>
2024-09-13 16:10:41 +0300

    update water model

--- src/db/models/water.js
+++ src/db/models/water.js
@@ -5,35 +5,56 @@ const waterRecordSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'users', 
         required: true 
-      },
-      amount: {
-        type: Number, 
-        required: true,
-        min: 0.05    
-      },
-      time: {
-        type: String,
-        required: true,
-        validate: {
-          validator: function (v) {
-            return /^([0-1]\d|2[0-3]):([0-5]\d)$/.test(v); 
-          },
-          message: 'Invalid time format (should be hh:mm)',
+    },
+    amount: {
+      type: Number, 
+      required: true,
+      min: 0.05    
+    },
+    date: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^(0[1-9]|[12]\d|3[01])$/.test(v); 
         },
+        message: 'Invalid day format (should be between 01 and 31)',
       },
-      date: {
-        type: String, 
-        required: true
+    },
+    month: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^(0[1-9]|1[0-2])$/.test(v); 
+        },
+        message: 'Invalid month format (should be between 01 and 12)',
       },
-      dailyWaterRate: {
-        type: Number, 
-        required: true,
-        min: 1,
-        max: 10
-      }
-},{
+    },
+    year: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^\d{4}$/.test(v); 
+        },
+        message: 'Invalid year format (should be a four-digit number)',
+      },
+    },
+    time: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^([0-1]\d|2[0-3]):([0-5]\d)$/.test(v); 
+        },
+        message: 'Invalid time format (should be hh:mm)',
+      },
+    },
+},
+{
     timestamps: true,
     versionKey: false, 
-  });
+});
 
-export const WaterCollection = model('WaterRecord', waterRecordSchema);
+export const WaterCollection = model('waters', waterRecordSchema);
