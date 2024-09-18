2bee6ae3e02c1328ea57e9dea75be65ce4259bc8
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-09-18 17:44:35 +0300

    Update reset-password.js

--- src/services/reset-password.js
+++ src/services/reset-password.js
@@ -1,9 +1,30 @@
-import createHttpError from "http-errors";
-import { UsersCollection } from "../db/models/user.js";
+import createHttpError from 'http-errors';
+import { UsersCollection } from '../db/models/user.js';
+import jwt from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const requestResetToken = async (email) => {
-    const user = await UsersCollection.findOne({ email });
-    if (!user) {
-        throw createHttpError(404, 'User not found');
-    }
+  const user = await UsersCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+
+    process.env.JWT_SECRET,
+
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  await sendEmail({
+    from: process.env.SMTP_FROM,
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+  });
 };
