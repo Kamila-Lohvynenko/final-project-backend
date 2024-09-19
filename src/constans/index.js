7a56c80a036977156c63dfeff0a62cc04d38d6e3
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-09-19 10:30:44 +0200

    Update index.js

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,16 +1,18 @@
 import path from 'node:path';
 
 // export const FIFTEEN_MINUTES = 15 * 60 * 1000; //15 minutes in milliseconds
-// export const FIFTEEN_MINUTES = 60 * 1000;
-export const FIFTEEN_MINUTES = 15 * 60 * 60 * 1000;
+export const FIFTEEN_MINUTES = 60 * 1000;
+// export const FIFTEEN_MINUTES = 15 * 60 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000; //30 days in milliseconds
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'src', 'tmp');
-export const UPLOAD_DIR = path.join(process.cwd(), 'src', 'public', 'userAvatars');
-
+export const UPLOAD_DIR = path.join(
+  process.cwd(),
+  'src',
+  'public',
+  'userAvatars',
+);
 
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 
-
-export const TEMPLATES_DIR = path.join(process.cwd(),
-    'src', 'templates');
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
