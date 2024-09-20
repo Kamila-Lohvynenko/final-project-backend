50345f959c175d67223a78129b1a7349253458a0
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-09-20 15:18:27 +0200

    Update water.js

--- src/services/water.js
+++ src/services/water.js
@@ -4,25 +4,34 @@ import { WaterCollection } from '../db/models/water.js';
 export const addWater = async (userId, amount, day, month, year, time) => {
   const [hour, minute] = time.split(':').map(Number);
 
-  if (hour < 0 || hour > 23 || minute < 0 || minute > 59 || isNaN(hour) || isNaN(minute)) {
-    throw new Error('Invalid time format. Use "HH:mm", where hours must be between 0-23 and minutes between 0-59');
+  if (
+    hour < 0 ||
+    hour > 23 ||
+    minute < 0 ||
+    minute > 59 ||
+    isNaN(hour) ||
+    isNaN(minute)
+  ) {
+    throw new Error(
+      'Invalid time format. Use "HH:mm", where hours must be between 0-23 and minutes between 0-59',
+    );
   }
-    const currentDate = new Date();
-    currentDate.setHours(0, 0, 0, 0); 
+  const currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
 
-    const inputDate = new Date(year, month - 1, day, hour, minute); 
-    inputDate.setHours(0, 0, 0, 0); 
-  
-    if (inputDate.toDateString() !== currentDate.toDateString()) {
-      throw new Error('You can only add water for today\'s date');
-    }
+  const inputDate = new Date(year, month - 1, day, hour, minute);
+  inputDate.setHours(0, 0, 0, 0);
+
+  if (inputDate.toDateString() !== currentDate.toDateString()) {
+    throw new Error("You can only add water for today's date");
+  }
   const waterRecord = await WaterCollection.create({
     userId,
     amount,
     day,
     month,
     year,
-    time
+    time,
   });
   return waterRecord;
 };
@@ -55,7 +64,7 @@ export const getWaterByDay = async (userId, day, month, year) => {
     throw new Error(404, 'User not found');
   }
 
-  const dailyNorma = user.dailyNorma * 1000;
+  const dailyNorma = user.dailyNorma;
   const percentage = Math.round((totalWater / dailyNorma) * 100);
 
   return {
